a03c1de9bbbadd6725d0774f19681b1896930498
raz010 <raz010@csiro.au>
2021-05-28 12:43:24 +1000

    Added global detector script that allowws gesture detection without prefab

---
+++ Assets/HandGestureAssets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public void SpawnObject(GameObject go)
+    {
+        Instantiate(go, transform.position, Quaternion.identity );
+    }
+}
